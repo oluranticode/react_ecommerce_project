0542e34805fea81207a6295a1c9d9582cb2dd168
youngtemmy <alukotemitope49@gmail.com>
2020-04-12 02:10:43 +0100

    after i created the sign in form

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInSignUp from  './pages/sign-in-sign-up/sign-in-sign-up.component';
 
 
 function App() {
     return (
       <div>
+      <Header />
       <Switch>
       <Route exact  path='/' component={Homepage}/>
-      <Route exact path='/shop' component={ShopPage}/>
+      <Route  path='/shop' component={ShopPage}/>
+      <Route  path='/signIn' component={SignInSignUp}/>
       </Switch>
 
       </div>
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component';
+import './sign-in.style.scss';
+
+class SignIn extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            email : '',
+            password: ''
+        }
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({ email: '', password:'' })
+        
+    }
+
+    handleChange = event => {
+        const {value, name} = event.target;
+
+        this.setState({ [name] : value })
+    }
+
+    render (){
+        return (
+            <div className="sign-in">
+            <h1>I already have an account</h1>
+            <span>sign in with email or login with your password</span>
+
+            <form onSubmit={this.handleSubmit}>
+            
+            <FormInput name="email" type="email" value={this.state.email} 
+            handleChange={this.handleChange}
+            label="email"
+             required/>
+            
+            <FormInput name="password" type="password" value={this.state.password} 
+            handleChange={this.handleChange} 
+            label="password"
+            required/>
+
+                <CustomButton type="submit"> SIGN IN </CustomButton>
+            </form>
+                <h1>{this.state.email}</h1>
+            </div>
+        );
+    }
+
+}
+
+export default SignIn;
+
+// this is first sign in / up form i created before importing Form=input
+
+/*
+<div className="sign-in">
+            <h1>I already have an account</h1>
+            <span>sign in with email or login with your password</span>
+
+            <form onSubmit={this.handleSubmit}>
+            
+            <label>Email: </label>
+            <input name="email" type="email" value={this.state.email} 
+            onChange={this.handleChange}
+            
+             required/>
+
+             <label>password: </label>
+            <input name="password" type="password" value={this.state.password} 
+            onChange={this.handleChange} 
+            required/>
+
+                <input type="submit" value='Form Submit' />
+            </form>
+                <h1>{this.state.email}</h1>
+            </div>
+            */
